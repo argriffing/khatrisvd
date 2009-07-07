48c9a15d2df006dda45f02c73bfe7084c77032c6
Alexander Griffing <argriffing@gmail.com>
2009-07-06 21:00:13 -0400

    added a real function to this module

--- khatrisvd/khorr.py
+++ khatrisvd/khorr.py
@@ -9,11 +9,28 @@ gene and each column represents a genetic line.
 
 import numpy as np
 
+import unittest
+
 def khatri_rao_square(M):
     """
     @param M: a numpy array
+    @return: the Khatri-Rao product of the matrix with itself
     """
-    np.vstack(np.kron(row, row) for row in M.T).T
+    return np.vstack(np.kron(row, row) for row in M.T).T
 
-def test():
-    print 'ohai'
+
+class TestMe(unittest.TestCase):
+
+    def test_khatri_rao_square(self):
+        M = np.array([[1,2,3],[4,5,6]])
+        expected = np.array([
+            [ 1,  4,  9],
+            [ 4, 10, 18],
+            [ 4, 10, 18],
+            [16, 25, 36]])
+        observed = khatri_rao_square(M)
+        self.assertTrue(np.allclose(observed, expected))
+
+
+if __name__ == '__main__':
+    unittest.main()
